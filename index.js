c8e3f85a0546e6e22c042bed4a7ce7c9aaca4a76
gayathri <gayathri.ramakrishna158@gmail.com>
2022-10-22 11:27:18 +0530

    backend project

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express")
+const { apicontroller } = require("./Controllers/data")
+
+const app = express()
+
+const dataRouter = require("./Routes/data")
+
+const cors = require("cors")
+
+app.use(cors());
+
+app.use("/api", dataRouter);
+
+app.listen(process.env.PORT || 8080, () =>{
+    console.log("connected");
+})
